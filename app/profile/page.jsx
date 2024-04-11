04e8ef81c2a55e397616ad05a9e3b808bb77bfbb
mashhoor-ahdal <mashhoorahdal2@gmail.com>
2024-04-11 16:58:12 +0530

    routes for delete , update

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,17 +2,52 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const { data: session } = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      console.log(data);
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((p) => p._d !== post.id);
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Profile
       name="my"
-      desc="welcome to your personalised profile"
-      post={[]}
+      desc="Welcome to your personalised profile"
+      data={myPosts}
       handleDelete={handleDelete}
       handleEdit={handleEdit}
     />
